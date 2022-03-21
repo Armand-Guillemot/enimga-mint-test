e39b6c77c1f9c8503c124d4edb405d57a6dad007
Armand Guillemot <armand.guillemot.56@gmail.com>
2022-03-21 11:12:18 +0100

    changes

--- js/packages/candy-machine-ui/src/Home.tsx
+++ js/packages/candy-machine-ui/src/Home.tsx
@@ -506,7 +506,7 @@ const Home = (props: HomeProps) => {
             display="block"
             style={{ marginTop: 7, color: 'grey' }}
           >
-            Powered by METAPLEX
+            Powered by ARCANCIA
           </Typography>
         </Paper>
       </Container>
